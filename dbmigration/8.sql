d042e475693294dca54fa3813d7e693c05ed951b
lillem4n <lilleman@larvit.se>
2016-09-13 18:54:30 +0200

    Changed afghanska to pashto

--- dbmigration/8.sql
+++ dbmigration/8.sql
@@ -7,5 +7,7 @@ INSERT INTO geo_langLabels VALUES("cmn", "eng", "Mandarin Chinese");
 INSERT INTO geo_langLabels VALUES("chi", "swe", "Kantonesiska");
 INSERT INTO geo_langLabels VALUES("chi", "eng", "Cantonese");
 
+UPDATE geo_langLabels SET label = 'pashto' WHERE langIso639_3 = 'pus' AND labelIso639_3 = 'swe';
+
 UPDATE geo_territoryLabels SET label = "Palestina", `labelShort` = NULL
 WHERE `terIso3166_1_alpha_2` = "PS" AND `labelIso639_3` = "swe";
