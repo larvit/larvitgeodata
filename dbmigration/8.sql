3823cf1275dea973a1cb8bd44205d3278edd381f
lillem4n <lilleman@larvit.se>
2016-09-13 18:52:05 +0200

    Removed the sami stuff

--- dbmigration/8.sql
+++ dbmigration/8.sql
@@ -6,8 +6,6 @@ INSERT INTO geo_langLabels VALUES("cmn", "swe", "mandarin");
 INSERT INTO geo_langLabels VALUES("cmn", "eng", "Mandarin Chinese");
 INSERT INTO geo_langLabels VALUES("chi", "swe", "Kantonesiska");
 INSERT INTO geo_langLabels VALUES("chi", "eng", "Cantonese");
-INSERT INTO geo_langLabels VALUES("sme", "swe", "Samiska");
-INSERT INTO geo_langLabels VALUES("sme", "eng", "Sami");
 
 UPDATE geo_territoryLabels SET label = "Palestina", `labelShort` = NULL
 WHERE `terIso3166_1_alpha_2` = "PS" AND `labelIso639_3` = "swe";
