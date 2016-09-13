7d1fb95ad544a6a8a2374bf27fcf662b61112fc1
lilleman <lilleman@larvit.se>
2016-09-13 15:45:15 +0200

    Added some labels and minor changes to db

---
+++ dbmigration/8.sql
@@ -0,0 +1,14 @@
+INSERT INTO geo_langs ('chi', NULL, 'living', 'individual');
+
+INSERT INTO geo_langLabels VALUES('prs', 'swe', 'dari');
+INSERT INTO geo_langLabels VALUES('prs', 'eng', 'Dari');
+INSERT INTO geo_langLabels VALUES('cmn', 'swe', 'mandarin');
+INSERT INTO geo_langLabels VALUES('cmn', 'eng', 'Mandarin Chinese');
+INSERT INTO geo_langLabels VALUES('pus', 'swe', 'Pashto');
+INSERT INTO geo_langLabels VALUES('chi', 'swe', 'Kantonesiska');
+INSERT INTO geo_langLabels VALUES('chi', 'eng', 'Cantonese');
+INSERT INTO geo_langLabels VALUES('sme', 'swe', 'Samiska');
+INSERT INTO geo_langLabels VALUES('sme', 'eng', 'Sami');
+
+UPDATE geo_territoryLabels SET label = 'Palestina', `labelShort` = NULL
+WHERE `terIso3166_1_alpha_2` = 'PS' AND `labelIso639_3` = 'swe';
